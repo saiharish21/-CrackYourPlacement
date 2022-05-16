0b83701b255e26191a6d003f34187cc6121ad24e
saiharish21 <99723842+saiharish21@users.noreply.github.com>
2022-05-16 09:26:51 +0530

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -1,1 +1,15 @@
-
+class Solution:
+     def twoSum(self, nums: List[int], target: int) -> List[int]:
+       dict={}
+       for i in range(0,len(nums)):
+            dict[nums[i]]=i
+       for i in range(0,len(nums)):
+            if target-nums[i] in dict:
+                x=dict[target-nums[i]]
+                if i!=x:
+                    return [i,x]
+        #    for i in range(0,len(nums)):
+    #      if target-nums[i] in nums:
+    #             x=nums.index(target-nums[i])
+    #             if i!=x:
+    #                 return [i,x]
